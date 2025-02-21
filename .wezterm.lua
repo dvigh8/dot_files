94327bbd7220ee6cb6763c16ed650ab81ee495b0
David Vigh <josephs.david.b@gmail.com>
2025-02-21 15:12:47 -0600

    init files

---
+++ .wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+local config = {
+	keys = {
+		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+		{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+		-- Make Option-Right equivalent to Alt-f; forward-word
+		{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+	},
+}
+config.color_scheme = "Dracula"
+config.font_size = 18.0
+return config
